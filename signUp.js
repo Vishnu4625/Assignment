2792e6a9ea4d18b292315066989b65f1d416e8e9
Vishnu4625 <vishnuprasad4625@gmail.com>
2021-11-13 17:41:39 +0530

    login & signup2

--- signUp.js
+++ signUp.js
@@ -1,8 +1,11 @@
 let email = document.getElementById("email");
-let pwd = document.getElementById("pwd");
+var pwd = document.getElementById("pwd");
+let mobileNo = document.getElementById("mobileNo");
 function validate(){
     let regexp1 = /^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
     let regexp2 = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
+    let regexp3 = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    
         if(regexp1.test(email.value)){
             // return true;
         }
@@ -10,9 +13,17 @@ function validate(){
             alert("Please enter email id in correct format");
             return false;
             
+        } 
+        if(mobileNo.value.match(regexp3)){
+	        // return true;      
+	    }
+        else{
+	        alert("Not a valid Phone Number");
+	        return false;
         }
+        
         if(pwd.value.match(regexp2)){
-            return true;
+            // return true;
 
         }
         else{
